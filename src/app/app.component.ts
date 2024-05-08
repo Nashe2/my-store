30327ef8b79977be8f118711e1bb95e1a11ffca3
Nashe Osorio <nasheog2@gmail.com>
2024-05-08 16:45:58 -0600

    LLamar un componente en otro componente.
    - Llame el componente img al componente app.
    
    - Se importa el componente img al componente app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImgComponent } from "./pages/img/img.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, ImgComponent]
 })
 export class AppComponent {
   title = 'my-store';
