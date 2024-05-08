26a671b2a51b783986950980a91051b355bc22c6
Nashe Osorio <nasheog2@gmail.com>
2024-05-08 16:31:42 -0600

    Componente.
    - Cree el componente img en la carpeta pages.
    
    - Redireccione la ruta img.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ImgComponent } from './pages/img/img.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'img',
+    component: ImgComponent,
+  }
+];
